da6948291f938a493a33bf7239d2128f09254cac
thisozaii <159558661+thisozaii@users.noreply.github.com>
2024-02-09 19:50:14 +0300

    Create JShell.java

---
+++ Bölümler/JShell.java
@@ -0,0 +1,3 @@
+// JShell, Java'nın etkileşimli kabuğudur ve kod parçalarını hızlıca değerlendirme ve test etme olanağı sağlar.
+// Burada, JShell'in temel kullanımı gösterilmektedir.
+System.out.println("Hello, JShell!");
